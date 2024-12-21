6e05b70d764fc49527398ff0a9d844e1a3d5b063
Firaol Mk <firaolmekbib511@gmail.com>
2024-12-22 02:31:34 +0300

    views of django

--- learndjango/pages/views.py
+++ learndjango/pages/views.py
@@ -5,15 +5,16 @@ from django.http import HttpResponse
 
 # home page
 def home(request):
-    return HttpResponse("<h1>Home Page</h1>") 
+    #return HttpResponse("<h1>Home Page</h1>")
+    return render(request, 'pages/home.html')
 # about as page
 def about(request):
-    return HttpResponse("<h1>About Us Page</h1>") 
+    return render(request, 'pages/about.html')
 
 # contact as page
 def contact(request):
-    return HttpResponse("<h1>Contact us Page</h1>") 
+    return render(request, 'pages/contact.html')
 
 # services page
 def services(request):
-    return HttpResponse("<h1>Services Page</h1>") 
+    return render(request, 'pages/services.html')
