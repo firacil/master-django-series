6c85b55ae7d0318cafeb51d632c62835703ebf5c
Firaol Mk <firaolmekbib511@gmail.com>
2024-12-22 04:20:57 +0300

    blog implemented

--- learndjango/blog/views.py
+++ learndjango/blog/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-# Blog
-def blog(request, article_id):
-    return HttpResponse(f"<h1>Blog {article_id}</h1>")
+# Blog_archive
+def blog_archive(request):
+    return render(request, 'blog/index.html')
+
+# Blog_post
+def blog_post(request, article_id):
+    return render(request, 'blog/post.html')
