da8468a778b95587c801cb1b8e696ee1008521f5
Firaol Mk <firaolmekbib511@gmail.com>
2024-12-22 09:16:44 +0300

    templates and views

--- learndjango/blog/views.py
+++ learndjango/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 
@@ -21,4 +21,22 @@ def blog_archive(request):
 
 # Blog_post
 def blog_post(request, article_id):
-    return render(request, 'blog/post.html')
+    
+    blog_post = [
+        {'id': 1, 'title': 'The Importance of Time Management', 'author': 'Firaol Mk', 'content': 'Learn how to manage your time effectively to boost productivity and reduce stress.', 'date': 'December 15, 2024' },
+        {'id': 2, 'title': '5 Tips for Staying Motivated', 'author': 'Ebisa N', 'content': 'Discover actionable tips to keep yourself motivated, even on challenging days.', 'date': 'April 01, 2024' },
+        {'id': 3, 'title': 'The Benefits of Journaling', 'author': 'Yisak O', 'content': 'Explore how maintaining a journal can enhance your personal growth and mindfulness.', 'date': 'January 01, 2025' },
+        {'id': 4, 'title': 'My Learning Journey at ALX', 'author': 'Firaol Mk', 'content': 'talk about how i tackle projects when i am in ALX software engineering program.', 'date': 'November 14, 2024' }
+    ]
+    
+    selected_post = None
+     
+    for post in blog_post:
+        if article_id == post['id']:
+            selected_post = post
+            break
+    
+    if selected_post == None:
+        raise Http404("Blog Not Found")
+             
+    return render(request, 'blog/post.html', selected_post)
